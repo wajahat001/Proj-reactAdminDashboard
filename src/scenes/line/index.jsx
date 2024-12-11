73ff8f0e36767810d83beebcd6c8f720b10cbc2b
wajahat001 <wajahatchaudy1640@gmail.com>
2024-12-11 08:35:54 +0500

    completed all side pages

--- src/scenes/line/index.jsx
+++ src/scenes/line/index.jsx
@@ -2,17 +2,17 @@ import { Box } from "@mui/material";
 import Header from "../../components/Header";
 import LineChart from "../../components/LineChart";
 
-import React from 'react'
+import React from "react";
 
 const Line = () => {
   return (
-    <Box m='20px' >
-        <Header title='LINECHART' subtitle='Simple LineChart' />
-        <Box height='75vh' >
-            <LineChart />
-        </Box>
+    <Box m="20px">
+      <Header title="LINECHART" subtitle="Simple LineChart" />
+      <Box height="75vh">
+        <LineChart />
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Line
+export default Line;
--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -52,13 +52,13 @@ const Team = () => {
                 ? colors.greenAccent[600]
                 : colors.greenAccent[700]
             }
-            borderRadius='4px'
+            borderRadius="4px"
           >
-            {access==="admin" && <AdminPanelSettingsOutlinedIcon />}
-            {access==='manager' && <SecurityOutlinedIcon />}
-            {access==='user' && <LockOpenOutlinedIcon />}
-            <Typography color={colors.grey[100]} sx={{ml:'5px'}} >
-                {access}
+            {access === "admin" && <AdminPanelSettingsOutlinedIcon />}
+            {access === "manager" && <SecurityOutlinedIcon />}
+            {access === "user" && <LockOpenOutlinedIcon />}
+            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+              {access}
             </Typography>
           </Box>
         );
@@ -67,31 +67,33 @@ const Team = () => {
   ];
 
   return (
-    <Box m='20px'>
+    <Box m="20px">
       <Header title="TEAM" subtitle="Managing the Team Members" />
-      <Box m='40px 0 0 0' height= "75vh"
-      sx={{
-        "& .MuiDataGrid-root":{
-          border: "none"
-        },
-        "& .MuiDataGrid-cell":{
-          borderBottom: "none"
-        },
-        "& .name-column--cell":{
-          color: colors.greenAccent[300]
-        },
-        "& .MuiDataGrid-columnHeader":{
-          backgroundColor: colors.blueAccent[700],
-          borderBottom: "none"
-        },
-        "& .MuiDataGrid-virtualScroller":{
-          backgroundColor: colors.primary[400]
-        },
-        "& .MuiDataGrid-footerContainer":{
-          borderTop: "none",
-          backgroundColor: colors.blueAccent[700]
-        }
-      }}
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeader": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+        }}
       >
         <DataGrid rows={mockDataTeam} columns={columns} />
       </Box>
